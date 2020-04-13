484d43658422235845965057bb06576606397fdf
ian <ikidder@gmail.com>
2020-04-13 10:54:41 -0400

    force rm of nested git

--- backup.sh
+++ backup.sh
@@ -7,6 +7,6 @@ cp -v ~/.bashrc ~/repos/configs
 cp -v ~/.bash_profile ~/repos/configs
 cp -vR ~/.emacs.d ~/repos/configs
 
-rm -r ~/repos/configs/.emacs.d/.git
-rm -r ~/repos/configs/.emacs.d/.github/
+rm -rf ~/repos/configs/.emacs.d/.git
+rm -rf ~/repos/configs/.emacs.d/.github/
 rm ~/repos/configs/.emacs.d/.gitignore
