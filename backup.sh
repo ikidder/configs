609d570508f82bb0593d524077bff0aa47b868cb
ian <ikidder@gmail.com>
2020-04-13 10:47:11 -0400

    removed nested git from backup

--- backup.sh
+++ backup.sh
@@ -3,6 +3,10 @@ set -Eeuo pipefail
 
 # brings config files into the current directory for check-in
 
-cp -v ~/.bashrc .
-cp -v ~/.bash_profile .
-cp -vR ~/.emacs.d .
+cp -v ~/.bashrc ~/repos/configs
+cp -v ~/.bash_profile ~/repos/configs
+cp -vR ~/.emacs.d ~/repos/configs
+
+rm -r ~/repos/configs/.emacs.d/.git
+rm -r ~/repos/configs/.emacs.d/.github/
+rm ~/repos/configs/.emacs.d/.gitignore
